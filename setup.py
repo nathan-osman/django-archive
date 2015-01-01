ad9166116ced36397fca45582a93ab5d188df18f
Nathan Osman <nathan@quickmediasolutions.com>
2014-12-31 17:00:24 -0800

    Once again fixed the module paths in setup.py.

--- setup.py
+++ setup.py
@@ -2,13 +2,17 @@ from distutils.core import setup
 
 setup(
     name='django_archive',
-    version='0.1.1',
+    version='0.1.2',
     description='Management command for creating compressed archives of DB tables and uploaded media',
     author='Nathan Osman',
     author_email='nathan@quickmediasolutions.com',
     url='https://github.com/nathan-osman/django-archive',
     license='MIT',
-    packages=['django_archive.management.commands'],
+    packages=[
+        'django_archive',
+        'django_archive.management',
+        'django_archive.management.commands',
+    ],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
