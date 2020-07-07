4050a8447854792eb3e5210484c15f012b0e371e
Nathan Osman <nathan@quickmediasolutions.com>
2020-07-06 22:53:45 -0700

    Update year in docs.

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Django Archive'
-copyright = u'2015, Nathan Osman'
+copyright = u'2020, Nathan Osman'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
