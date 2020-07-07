c211f25a115dec3f2b16b9b304358f3c01e6e761
Nathan Osman <nathan@quickmediasolutions.com>
2020-07-06 22:26:28 -0700

    Refactor to use new archival classes.

--- django_archive/management/commands/archive.py
+++ django_archive/management/commands/archive.py
@@ -2,40 +2,15 @@ from datetime import datetime
 from io import BytesIO
 from json import dump
 from os import path
-from tarfile import TarInfo, TarFile
 
 from django.apps.registry import apps
 from django.conf import settings
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.db import models
-from django.utils.encoding import smart_bytes
 
 from ... import __version__
-
-
-class MixedIO(BytesIO):
-    """
-    A BytesIO that accepts and encodes Unicode data.
-
-    This class was born out of a need for a BytesIO that would accept writes of
-    both bytes and Unicode data - allowing identical usage from both Python 2
-    and Python 3.
-    """
-
-    def rewind(self):
-        """
-        Seeks to the beginning and returns the size.
-        """
-        size = self.tell()
-        self.seek(0)
-        return size
-
-    def write(self, data):
-        """
-        Writes the provided data, converting Unicode to bytes as needed.
-        """
-        BytesIO.write(self, smart_bytes(data))
+from ...archivers import get_archiver, TARBALL_BZ2
 
 
 class Command(BaseCommand):
@@ -45,31 +20,38 @@ class Command(BaseCommand):
 
     help = "Create a compressed archive of database tables and uploaded media."
 
-    def handle(self, *args, **kwargs):
+    @staticmethod
+    def _get_base_filename(archiver, fmt):
         """
-        Process the command.
+        Derive the base filename for the archive
         """
-        with self._create_archive() as tar:
-            self._dump_db(tar)
-            self._dump_files(tar)
-            self._dump_meta(tar)
-        self.stdout.write("Backup completed.")
-
-    def _create_archive(self):
-        """
-        Create the archive and return the TarFile.
-        """
-        filename = getattr(settings, 'ARCHIVE_FILENAME', '%Y-%m-%d--%H-%M-%S')
-        fmt = getattr(settings, 'ARCHIVE_FORMAT', 'bz2')
-        absolute_path = path.join(
+        return path.join(
             getattr(settings, 'ARCHIVE_DIRECTORY', ''),
-            '%s.tar.%s' % (datetime.today().strftime(filename), fmt)
+            "{}.{}".format(
+                datetime.today().strftime(
+                    getattr(
+                        settings,
+                        'ARCHIVE_FILENAME',
+                        '%Y-%m-%d--%H-%M-%S',
+                    ),
+                ),
+                archiver.get_extension(fmt),
+            ),
         )
-        return TarFile.open(absolute_path, 'w:%s' % fmt)
 
-    def _dump_db(self, tar):
+    @staticmethod
+    def _add_to_archive(archive, filename, fileobj):
         """
-        Dump the rows in each model to the archive.
+        Add the filename / fileobj to the archive
+        """
+        size = fileobj.tell()
+        fileobj.seek(0)
+        archive.add(filename, size, fileobj)
+
+    @staticmethod
+    def _dump_db(archive):
+        """
+        Write database data to the archive
         """
 
         # Determine the list of models to exclude
@@ -79,45 +61,72 @@ class Command(BaseCommand):
             'sessions.Session',
         ))
 
-        # Dump the tables to a MixedIO
-        data = MixedIO()
-        call_command('dumpdata', all=True, format='json', exclude=exclude, stdout=data)
-        info = TarInfo('data.json')
-        info.size = data.rewind()
-        tar.addfile(info, data)
+        # TODO: use a temp file to avoid excess memory usage
 
-    def _dump_files(self, tar):
+        # Dump the tables
+        data = BytesIO()
+        call_command(
+            'dumpdata',
+            all=True,
+            format='json',
+            exclude=exclude,
+            stdout=data,
+        )
+
+        # Write the data to the archive
+        Command._add_to_archive(archive, 'data.json', data)
+
+    @staticmethod
+    def _dump_files(archive):
         """
-        Dump all uploaded media to the archive.
+        Dump all uploaded media to the archive
         """
 
         # Loop through all models and find FileFields
         for model in apps.get_models():
 
-            # Get the name of all file fields in the model
+            # Find all FileFields in the model
             field_names = []
             for field in model._meta.fields:
                 if isinstance(field, models.FileField):
                     field_names.append(field.name)
 
-            # If any were found, loop through each row
-            if len(field_names):
-                for row in model.objects.all():
-                    for field_name in field_names:
-                        field = getattr(row, field_name)
-                        if field:
-                            field.open()
-                            info = TarInfo(field.name)
-                            info.size = field.size
-                            tar.addfile(info, field)
-                            field.close()
+            # If there aren't any FileFields, quit
+            if not field_names:
+                return
 
-    def _dump_meta(self, tar):
+            # Loop through each row, saving the files
+            for row in model.objects.all():
+                for field_name in field_names:
+                    field = getattr(row, field_name)
+                    if field:
+                        with field as field:
+                            archive.addfile(field.name, field.size, field)
+
+    @staticmethod
+    def _dump_meta(archive):
         """
-        Dump metadata to the archive.
+        Dump metadata to the archive
         """
-        data = MixedIO()
+        data = BytesIO()
         dump({'version': __version__}, data)
-        info = TarInfo('meta.json')
-        info.size = data.rewind()
-        tar.addfile(info, data)
+        Command._add_to_archive(archive, 'meta.json', data)
+
+    # pylint: disable=unused-argument
+    def handle(self, *args, **kwargs):
+        """
+        Process the command
+        """
+        fmt = getattr(settings, 'ARCHIVE_FORMAT', TARBALL_BZ2)
+        archiver = get_archiver(fmt)
+        filename = Command._get_base_filename(archiver, fmt)
+        with open(filename, 'w') as fileobj:
+            with archiver(fileobj, fmt) as archive:
+                Command._dump_db(archive)
+                Command._dump_files(archive)
+                Command._dump_meta(archive)
+        self.stdout.write(
+            self.style.SUCCESS(
+                "Successfully wrote {}".format(archive.get_filename),
+            ),
+        )
