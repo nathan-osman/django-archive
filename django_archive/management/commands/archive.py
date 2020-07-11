b2eeb849f1c37e08aacb313e8a233c1e4e331578
Nathan Osman <nathan@quickmediasolutions.com>
2020-07-11 13:43:49 -0700

    Refactor archive command to use the new MixedModeTemporaryFile class.

--- django_archive/management/commands/archive.py
+++ django_archive/management/commands/archive.py
@@ -1,5 +1,9 @@
+"""
+Create an archive
+"""
+
+from contextlib import contextmanager
 from datetime import datetime
-from io import BytesIO
 from json import dump
 from os import path
 
@@ -11,6 +15,7 @@ from django.db import models
 
 from ... import __version__
 from ...archivers import get_archiver, TARBALL_BZ2
+from ...util.file import MixedModeTemporaryFile
 
 
 class Command(BaseCommand):
@@ -20,11 +25,14 @@ class Command(BaseCommand):
 
     help = "Create a compressed archive of database tables and uploaded media."
 
+    _DEFAULT_ARCHIVE_EXCLUDE = (
+        'auth.Permission',
+        'contenttypes.ContentType',
+        'sessions.Session',
+    )
+
     @staticmethod
-    def _get_base_filename(archiver, fmt):
-        """
-        Derive the base filename for the archive
-        """
+    def _get_filename(archiver, fmt):
         return path.join(
             getattr(settings, 'ARCHIVE_DIRECTORY', ''),
             "{}.{}".format(
@@ -40,77 +48,56 @@ class Command(BaseCommand):
         )
 
     @staticmethod
-    def _add_to_archive(archive, filename, fileobj):
-        """
-        Add the filename / fileobj to the archive
-        """
-        size = fileobj.tell()
-        fileobj.seek(0)
-        archive.add(filename, size, fileobj)
+    @contextmanager
+    def _write_to_archive(archive, filename):
+        with MixedModeTemporaryFile() as tempfile:
+            with tempfile.open('w') as fileobj:
+                yield fileobj
+            tempfile.rewind()
+            with tempfile.open('rb') as fileobj:
+                archive.add(filename, tempfile.size(), fileobj)
+
+    @staticmethod
+    def _dump_meta(archive):
+        with Command._write_to_archive(archive, 'meta.json') as fileobj:
+            dump(
+                {
+                    'version': __version__,
+                },
+                fileobj,
+                indent=2,
+            )
 
     @staticmethod
     def _dump_db(archive):
-        """
-        Write database data to the archive
-        """
-
-        # Determine the list of models to exclude
-        exclude = getattr(settings, 'ARCHIVE_EXCLUDE', (
-            'auth.Permission',
-            'contenttypes.ContentType',
-            'sessions.Session',
-        ))
-
-        # TODO: use a temp file to avoid excess memory usage
-
-        # Dump the tables
-        data = BytesIO()
-        call_command(
-            'dumpdata',
-            all=True,
-            format='json',
-            exclude=exclude,
-            stdout=data,
-        )
-
-        # Write the data to the archive
-        Command._add_to_archive(archive, 'data.json', data)
+        with Command._write_to_archive(archive, 'data.json') as fileobj:
+            call_command(
+                'dumpdata',
+                all=True,
+                format='json',
+                exclude=getattr(
+                    settings,
+                    'ARCHIVE_EXCLUDE',
+                    Command._DEFAULT_ARCHIVE_EXCLUDE,
+                ),
+                stdout=fileobj,
+            )
 
     @staticmethod
     def _dump_files(archive):
-        """
-        Dump all uploaded media to the archive
-        """
-
-        # Loop through all models and find FileFields
         for model in apps.get_models():
-
-            # Find all FileFields in the model
             field_names = []
             for field in model._meta.fields:
                 if isinstance(field, models.FileField):
                     field_names.append(field.name)
-
-            # If there aren't any FileFields, quit
             if not field_names:
                 return
-
-            # Loop through each row, saving the files
             for row in model.objects.all():
                 for field_name in field_names:
                     field = getattr(row, field_name)
                     if field:
                         with field as field:
                             archive.addfile(field.name, field.size, field)
-
-    @staticmethod
-    def _dump_meta(archive):
-        """
-        Dump metadata to the archive
-        """
-        data = BytesIO()
-        dump({'version': __version__}, data)
-        Command._add_to_archive(archive, 'meta.json', data)
 
     # pylint: disable=unused-argument
     def handle(self, *args, **kwargs):
@@ -119,14 +106,15 @@ class Command(BaseCommand):
         """
         fmt = getattr(settings, 'ARCHIVE_FORMAT', TARBALL_BZ2)
         archiver = get_archiver(fmt)
-        filename = Command._get_base_filename(archiver, fmt)
-        with open(filename, 'w') as fileobj:
-            with archiver(fileobj, fmt) as archive:
+        filename = Command._get_filename(archiver, fmt)
+        with open(filename, 'wb') as fileobj:
+            archive = archiver(fileobj, fmt)
+            with archive:
+                Command._dump_meta(archive)
                 Command._dump_db(archive)
                 Command._dump_files(archive)
-                Command._dump_meta(archive)
         self.stdout.write(
             self.style.SUCCESS(
-                "Successfully wrote {}".format(archive.get_filename),
+                "Successfully wrote {}".format(filename),
             ),
         )
