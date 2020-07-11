9ff5ba29b24921c62aa4e86408c3fca0d291e8e0
Nathan Osman <nathan@quickmediasolutions.com>
2020-07-11 14:55:35 -0700

    Ensure that creating both tarball and zip archives is tested.

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1,6 +1,10 @@
 from django.core.management import call_command
 from django.test import TestCase
 
+from django_archive import archivers
+
+from .sample.models import Sample
+
 
 class ArchiveTestCase(TestCase):
     """
@@ -8,11 +12,21 @@ class ArchiveTestCase(TestCase):
     """
 
     def setUp(self):
-        call_command(
-            'archive',
-        )
+        """
+        Create an empty model instance
+        """
+        Sample().save()
 
-    def test_works(self):
+    def test_archive_tar(self):
         """
-        Test that the command actually works
+        Test that an uncompressed tarball is created
         """
+        with self.settings(ARCHIVE_FORMAT=archivers.TARBALL):
+            call_command('archive')
+
+    def test_archive_zip(self):
+        """
+        Test that a ZIP file is created
+        """
+        with self.settings(ARCHIVE_FORMAT=archivers.ZIP):
+            call_command('archive')
