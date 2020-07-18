5425c2419b7365969ea8b211432858d599214201
Nathan Osman <nathan@quickmediasolutions.com>
2020-07-18 16:17:21 -0700

    Update test to ensure attached files are present in archives.

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -1,5 +1,6 @@
 from json import load
 
+from django.core.files.base import ContentFile
 from django_archive import __version__
 
 from .base import BaseArchiveTestCase
@@ -11,18 +12,28 @@ class ArchiveTestCase(BaseArchiveTestCase):
     Test that the archive command includes correct data in the archive
     """
 
+    _ATTACHMENT_FILENAME = 'sample.txt'
+    _ATTACHMENT_CONTENT = b'sample'
+
     def setUp(self):
-        Sample().save()
+        sample = Sample()
+        sample.attachment.save(
+            self._ATTACHMENT_FILENAME,
+            ContentFile(self._ATTACHMENT_CONTENT),
+        )
         super().setUp()
 
     def test_data(self):
         """
-        Confirm that the model was archived
+        Confirm that the model and attached files were archived
         """
         with self.tarfile.extractfile('data.json') as fileobj:
             data = load(fileobj)
-            self.assertEqual(len(data), 1)
-            self.assertEqual(data[0]['model'], 'sample.sample')
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]['model'], 'sample.sample')
+        with self.tarfile.extractfile(data[0]['fields']['attachment']) as fileobj:
+            content = fileobj.read()
+        self.assertEqual(content, self._ATTACHMENT_CONTENT)
 
     def test_meta(self):
         """
