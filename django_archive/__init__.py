db22595ef50acccedf75032cb77e1a00f5a59f79
Nathan Osman <nathan@quickmediasolutions.com>
2020-07-25 17:45:42 -0700

    Add AppConfig for the app.

--- django_archive/__init__.py
+++ django_archive/__init__.py
@@ -1,3 +1,5 @@
 __title__ = 'django_archive'
 __version__ = '0.2.0'
 __author__ = 'Nathan Osman'
+
+default_app_config = 'django_archive.apps.DjangoArchiveConfig'
---
+++ django_archive/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class DjangoArchiveConfig(AppConfig):
+    """
+    Configuration for the django_archive app
+    """
+
+    name = 'django_archive'
+    verbose_name = "Django Archive"
